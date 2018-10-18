c5efe46ef4497966ec9d73088d28735b513ac6dc
Avesh Agarwal <avesh.ncsu@gmail.com>
2018-10-18 13:04:29 -0400

    Add operator status reporting to CVO.

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -5,30 +5,40 @@ import (
 	"os"
 	"time"
 
-	"github.com/openshift/cluster-kube-scheduler-operator/pkg/apis/kubescheduler/v1alpha1"
-	"github.com/openshift/cluster-kube-scheduler-operator/pkg/operator/v311_00_assets"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 
+	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
+	"github.com/openshift/cluster-kube-scheduler-operator/pkg/apis/kubescheduler/v1alpha1"
 	operatorconfigclient "github.com/openshift/cluster-kube-scheduler-operator/pkg/generated/clientset/versioned"
 	operatorsv1alpha1client "github.com/openshift/cluster-kube-scheduler-operator/pkg/generated/clientset/versioned/typed/kubescheduler/v1alpha1"
 	operatorclientinformers "github.com/openshift/cluster-kube-scheduler-operator/pkg/generated/informers/externalversions"
+	"github.com/openshift/cluster-kube-scheduler-operator/pkg/operator/v311_00_assets"
+	"github.com/openshift/library-go/pkg/operator/status"
 )
 
 func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
 	kubeClient, err := kubernetes.NewForConfig(clientConfig)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	operatorConfigClient, err := operatorconfigclient.NewForConfig(clientConfig)
 	if err != nil {
-		panic(err)
+		return err
 	}
+
+	dynamicClient, err := dynamic.NewForConfig(clientConfig)
+	if err != nil {
+		return err
+	}
+	ensureOperatorConfigExists(operatorConfigClient.KubeschedulerV1alpha1(), "v3.11.0/kube-scheduler/operator-config.yaml")
 
 	operatorConfigInformers := operatorclientinformers.NewSharedInformerFactory(operatorConfigClient, 10*time.Minute)
 	kubeInformersNamespaced := informers.NewFilteredSharedInformerFactory(kubeClient, 10*time.Minute, targetNamespaceName, nil)
@@ -42,12 +52,20 @@ func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
 		kubeClient.RbacV1(),
 	)
 
-	ensureOperatorConfigExists(operator.operatorConfigClient, "v3.11.0/kube-scheduler/operator-config.yaml")
+	clusterOperatorStatus := status.NewClusterOperatorStatusController(
+		"openshift-kube-scheduler",
+		"openshift-kube-scheduler",
+		dynamicClient,
+		&operatorStatusProvider{informers: operatorConfigInformers},
+	)
 
 	operatorConfigInformers.Start(stopCh)
 	kubeInformersNamespaced.Start(stopCh)
 
-	operator.Run(1, stopCh)
+	go operator.Run(1, stopCh)
+	go clusterOperatorStatus.Run(1, stopCh)
+
+	<-stopCh
 	return fmt.Errorf("stopped")
 }
 
@@ -93,4 +111,21 @@ func ensureOperatorConfigExists(client operatorsv1alpha1client.KubeSchedulerOper
 			panic(err)
 		}
 	}
+}
+
+type operatorStatusProvider struct {
+	informers operatorclientinformers.SharedInformerFactory
+}
+
+func (p *operatorStatusProvider) Informer() cache.SharedIndexInformer {
+	return p.informers.Kubescheduler().V1alpha1().KubeSchedulerOperatorConfigs().Informer()
+}
+
+func (p *operatorStatusProvider) CurrentStatus() (operatorv1alpha1.OperatorStatus, error) {
+	instance, err := p.informers.Kubescheduler().V1alpha1().KubeSchedulerOperatorConfigs().Lister().Get("instance")
+	if err != nil {
+		return operatorv1alpha1.OperatorStatus{}, err
+	}
+
+	return instance.Status.OperatorStatus, nil
 }
