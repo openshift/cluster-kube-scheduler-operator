b34ef2882b6abe00bfcb84634c362bb262f83b08
Avesh Agarwal <avesh.ncsu@gmail.com>
2018-10-31 14:26:38 -0400

    Use openshift/library-go helper function for config check.

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -2,13 +2,9 @@ package operator
 
 import (
 	"fmt"
-	"os"
 	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
@@ -18,10 +14,10 @@ import (
 	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
 	"github.com/openshift/cluster-kube-scheduler-operator/pkg/apis/kubescheduler/v1alpha1"
 	operatorconfigclient "github.com/openshift/cluster-kube-scheduler-operator/pkg/generated/clientset/versioned"
-	operatorsv1alpha1client "github.com/openshift/cluster-kube-scheduler-operator/pkg/generated/clientset/versioned/typed/kubescheduler/v1alpha1"
 	operatorclientinformers "github.com/openshift/cluster-kube-scheduler-operator/pkg/generated/informers/externalversions"
 	"github.com/openshift/cluster-kube-scheduler-operator/pkg/operator/v311_00_assets"
 	"github.com/openshift/library-go/pkg/operator/status"
+	"github.com/openshift/library-go/pkg/operator/v1alpha1helpers"
 )
 
 func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
@@ -38,10 +34,16 @@ func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
 	if err != nil {
 		return err
 	}
-	ensureOperatorConfigExists(operatorConfigClient.KubeschedulerV1alpha1(), "v3.11.0/kube-scheduler/operator-config.yaml")
 
 	operatorConfigInformers := operatorclientinformers.NewSharedInformerFactory(operatorConfigClient, 10*time.Minute)
 	kubeInformersNamespaced := informers.NewFilteredSharedInformerFactory(kubeClient, 10*time.Minute, targetNamespaceName, nil)
+
+	v1alpha1helpers.EnsureOperatorConfigExists(
+		dynamicClient,
+		v311_00_assets.MustAsset("v3.11.0/kube-scheduler/operator-config.yaml"),
+		schema.GroupVersionResource{Group: v1alpha1.GroupName, Version: "v1alpha1", Resource: "kubescheduleroperatorconfigs"},
+		v1alpha1helpers.GetImageEnv,
+	)
 
 	operator := NewKubeSchedulerOperator(
 		operatorConfigInformers.Kubescheduler().V1alpha1().KubeSchedulerOperatorConfigs(),
@@ -67,50 +69,6 @@ func RunOperator(clientConfig *rest.Config, stopCh <-chan struct{}) error {
 
 	<-stopCh
 	return fmt.Errorf("stopped")
-}
-
-func ensureOperatorConfigExists(client operatorsv1alpha1client.KubeSchedulerOperatorConfigsGetter, path string) {
-	v1alpha1Scheme := runtime.NewScheme()
-	v1alpha1.Install(v1alpha1Scheme)
-	v1alpha1Codecs := serializer.NewCodecFactory(v1alpha1Scheme)
-	operatorConfigBytes := v311_00_assets.MustAsset(path)
-	operatorConfigObj, err := runtime.Decode(v1alpha1Codecs.UniversalDecoder(v1alpha1.GroupVersion), operatorConfigBytes)
-	if err != nil {
-		panic(err)
-	}
-	requiredOperatorConfig, ok := operatorConfigObj.(*v1alpha1.KubeSchedulerOperatorConfig)
-	if !ok {
-		panic(fmt.Sprintf("unexpected object in %s: %t", path, operatorConfigObj))
-	}
-
-	hasImageEnvVar := false
-	if imagePullSpecFromEnv := os.Getenv("IMAGE"); len(imagePullSpecFromEnv) > 0 {
-		hasImageEnvVar = true
-		requiredOperatorConfig.Spec.ImagePullSpec = imagePullSpecFromEnv
-	}
-
-	existing, err := client.KubeSchedulerOperatorConfigs().Get(requiredOperatorConfig.Name, metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		if _, err := client.KubeSchedulerOperatorConfigs().Create(requiredOperatorConfig); err != nil {
-			panic(err)
-		}
-		return
-	}
-	if err != nil {
-		panic(err)
-	}
-
-	if !hasImageEnvVar {
-		return
-	}
-
-	// If ImagePullSpec changed, update the existing config instance
-	if existing.Spec.ImagePullSpec != requiredOperatorConfig.Spec.ImagePullSpec {
-		existing.Spec.ImagePullSpec = requiredOperatorConfig.Spec.ImagePullSpec
-		if _, err := client.KubeSchedulerOperatorConfigs().Update(existing); err != nil {
-			panic(err)
-		}
-	}
 }
 
 type operatorStatusProvider struct {
