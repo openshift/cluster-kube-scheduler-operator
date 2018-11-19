8411062d6c13b9abf77e071ce8f3323bd7395c33
Avesh Agarwal <avesh.ncsu@gmail.com>
2018-11-19 17:49:37 -0500

    Update operator status and config observers.

--- pkg/operator/staticpod_interface.go
+++ pkg/operator/staticpod_interface.go
@@ -1,10 +1,11 @@
 package operator
 
 import (
-	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
+	"k8s.io/client-go/tools/cache"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
 	operatorconfigclientv1alpha1 "github.com/openshift/cluster-kube-scheduler-operator/pkg/generated/clientset/versioned/typed/kubescheduler/v1alpha1"
 	operatorclientinformers "github.com/openshift/cluster-kube-scheduler-operator/pkg/generated/informers/externalversions"
-	"k8s.io/client-go/tools/cache"
 )
 
 type staticPodOperatorClient struct {
@@ -16,7 +17,7 @@ func (c *staticPodOperatorClient) Informer() cache.SharedIndexInformer {
 	return c.informers.Kubescheduler().V1alpha1().KubeSchedulerOperatorConfigs().Informer()
 }
 
-func (c *staticPodOperatorClient) Get() (*operatorv1alpha1.OperatorSpec, *operatorv1alpha1.StaticPodOperatorStatus, string, error) {
+func (c *staticPodOperatorClient) Get() (*operatorv1.OperatorSpec, *operatorv1.StaticPodOperatorStatus, string, error) {
 	instance, err := c.informers.Kubescheduler().V1alpha1().KubeSchedulerOperatorConfigs().Lister().Get("instance")
 	if err != nil {
 		return nil, nil, "", err
@@ -25,7 +26,7 @@ func (c *staticPodOperatorClient) Get() (*operatorv1alpha1.OperatorSpec, *operat
 	return &instance.Spec.OperatorSpec, &instance.Status.StaticPodOperatorStatus, instance.ResourceVersion, nil
 }
 
-func (c *staticPodOperatorClient) UpdateStatus(resourceVersion string, status *operatorv1alpha1.StaticPodOperatorStatus) (*operatorv1alpha1.StaticPodOperatorStatus, error) {
+func (c *staticPodOperatorClient) UpdateStatus(resourceVersion string, status *operatorv1.StaticPodOperatorStatus) (*operatorv1.StaticPodOperatorStatus, error) {
 	original, err := c.informers.Kubescheduler().V1alpha1().KubeSchedulerOperatorConfigs().Lister().Get("instance")
 	if err != nil {
 		return nil, err
@@ -43,10 +44,10 @@ func (c *staticPodOperatorClient) UpdateStatus(resourceVersion string, status *o
 }
 
 // TODO collapse this onto get
-func (c *staticPodOperatorClient) CurrentStatus() (operatorv1alpha1.OperatorStatus, error) {
+func (c *staticPodOperatorClient) CurrentStatus() (operatorv1.OperatorStatus, error) {
 	instance, err := c.informers.Kubescheduler().V1alpha1().KubeSchedulerOperatorConfigs().Lister().Get("instance")
 	if err != nil {
-		return operatorv1alpha1.OperatorStatus{}, err
+		return operatorv1.OperatorStatus{}, err
 	}
 
 	return instance.Status.OperatorStatus, nil
