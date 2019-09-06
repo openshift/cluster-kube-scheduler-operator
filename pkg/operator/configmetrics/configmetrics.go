45dbf1ed529e6e2d3c2869a93f49748a0e37a030
Mike Dame <mdame@redhat.com>
2019-09-05 20:56:33 -0400

    Add master_schedulable metric

---
+++ pkg/operator/configmetrics/configmetrics.go
@@ -0,0 +1,42 @@
+package configmetrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+
+	configinformers "github.com/openshift/client-go/config/informers/externalversions"
+	configlisters "github.com/openshift/client-go/config/listers/config/v1"
+)
+
+func Register(configInformer configinformers.SharedInformerFactory) {
+	prometheus.MustRegister(&configMetrics{
+		configLister: configInformer.Config().V1().Schedulers().Lister(),
+		config: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "cluster_master_schedulable",
+			Help: "Reports whether the cluster master nodes are schedulable.",
+		}),
+	})
+}
+
+// configMetrics implements metrics gathering for this component.
+type configMetrics struct {
+	configLister configlisters.SchedulerLister
+	config       prometheus.Gauge
+}
+
+// Describe reports the metadata for metrics to the prometheus collector.
+func (m *configMetrics) Describe(ch chan<- *prometheus.Desc) {
+	ch <- m.config.Desc()
+}
+
+// Collect calculates metrics from the cached config and reports them to the prometheus collector.
+func (m *configMetrics) Collect(ch chan<- prometheus.Metric) {
+	if config, err := m.configLister.Get("cluster"); err == nil {
+		var g prometheus.Gauge
+		var value float64
+		if config.Spec.MastersSchedulable {
+			value = 1
+		}
+		g.Set(value)
+		ch <- g
+	}
+}
