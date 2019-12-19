a4a61cd711346f2a9e89b2c19bed87ea5d9b194b
Tomas Nozicka <tnozicka@gmail.com>
2019-12-19 11:42:48 +0100

    Bump: for library-go with go mod based verify-deps

--- vendor/go.uber.org/zap/global_go112.go
+++ vendor/go.uber.org/zap/global_go112.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 Uber Technologies, Inc.
+// Copyright (c) 2019 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,38 +18,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package atomic
+// See #682 for more information.
+// +build go1.12
 
-// Error is an atomic type-safe wrapper around Value for errors
-type Error struct{ v Value }
+package zap
 
-// errorHolder is non-nil holder for error object.
-// atomic.Value panics on saving nil object, so err object needs to be
-// wrapped with valid object first.
-type errorHolder struct{ err error }
-
-// NewError creates new atomic error object
-func NewError(err error) *Error {
-	e := &Error{}
-	if err != nil {
-		e.Store(err)
-	}
-	return e
-}
-
-// Load atomically loads the wrapped error
-func (e *Error) Load() error {
-	v := e.v.Load()
-	if v == nil {
-		return nil
-	}
-
-	eh := v.(errorHolder)
-	return eh.err
-}
-
-// Store atomically stores error.
-// NOTE: a holder object is allocated on each Store call.
-func (e *Error) Store(err error) {
-	e.v.Store(errorHolder{err: err})
-}
+const _stdLogDefaultDepth = 1
