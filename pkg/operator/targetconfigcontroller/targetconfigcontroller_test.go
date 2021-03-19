e837452b29cd52b93748017b46bda4274a775260
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2021-03-19 14:25:00 +0100

    initial support for flags for kubernetes-scheduler
    It allows for passing additional arguments to the kubernetes-scheduler.
    For now only via unsupportedConfigOverrides.
    
    For example to switch KS to use a preferred host (for now only localhost is supported) the following unsupported-kube-api-over-localhost must be set to true:
    
    unsupportedConfigOverrides:
      arguments:
        unsupported-kube-api-over-localhost:
          - "true"

--- pkg/operator/targetconfigcontroller/targetconfigcontroller_test.go
+++ pkg/operator/targetconfigcontroller/targetconfigcontroller_test.go
@@ -1,16 +1,30 @@
 package targetconfigcontroller
 
 import (
+	"context"
+	"io/ioutil"
+	"os"
 	"reflect"
 	"testing"
 
 	"k8s.io/apimachinery/pkg/util/sets"
 
 	configv1 "github.com/openshift/api/config/v1"
-	configlistersv1 "github.com/openshift/client-go/config/listers/config/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/diff"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/cache"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+	configlistersv1 "github.com/openshift/client-go/config/listers/config/v1"
+	"github.com/openshift/library-go/pkg/operator/events"
 )
+
+var codec = scheme.Codecs.LegacyCodec(scheme.Scheme.PrioritizedVersionsAllGroups()...)
 
 func TestCheckForFeatureGates(t *testing.T) {
 	tests := []struct {
@@ -100,4 +114,119 @@ func TestGetSortedFeatureGates(t *testing.T) {
 	if expectedFeatureGateString != actualFeatureGateString {
 		t.Fatalf("Expected %v as featuregate string but got %v", expectedFeatureGateString, actualFeatureGateString)
 	}
+}
+
+var unsupportedConfigOverridesSchedulerArgJSON = `
+{
+  "arguments": {
+      "master": "https://localhost:1234"
+  }
+}
+`
+
+var unsupportedConfigOverridesMultipleSchedulerArgsJSON = `
+{
+  "arguments": {
+      "master": "https://localhost:1234",
+      "unsupported-kube-api-over-localhost": "true"
+  }
+}
+`
+
+func TestManagePodToLatest(t *testing.T) {
+	scenarios := []struct {
+		name       string
+		goldenFile string
+		operator   *operatorv1.KubeScheduler
+	}{
+
+		// scenario 1
+		{
+			name:       "happy path: a pod with default values is created",
+			goldenFile: "./testdata/ks_pod_scenario_1.yaml",
+			operator:   &operatorv1.KubeScheduler{Spec: operatorv1.KubeSchedulerSpec{StaticPodOperatorSpec: operatorv1.StaticPodOperatorSpec{OperatorSpec: operatorv1.OperatorSpec{}}}},
+		},
+
+		// scenario 2
+		{
+			name:       "an unsupported flag is passed directly to the kube scheduler",
+			goldenFile: "./testdata/ks_pod_scenario_2.yaml",
+			operator: &operatorv1.KubeScheduler{Spec: operatorv1.KubeSchedulerSpec{StaticPodOperatorSpec: operatorv1.StaticPodOperatorSpec{OperatorSpec: operatorv1.OperatorSpec{
+				UnsupportedConfigOverrides: runtime.RawExtension{Raw: []byte(unsupportedConfigOverridesSchedulerArgJSON)},
+			}}}},
+		},
+
+		// scenario 3
+		{
+			name:       "unsupported flags are passed directly to the kube scheduler",
+			goldenFile: "./testdata/ks_pod_scenario_3.yaml",
+			operator: &operatorv1.KubeScheduler{Spec: operatorv1.KubeSchedulerSpec{StaticPodOperatorSpec: operatorv1.StaticPodOperatorSpec{OperatorSpec: operatorv1.OperatorSpec{
+				UnsupportedConfigOverrides: runtime.RawExtension{Raw: []byte(unsupportedConfigOverridesMultipleSchedulerArgsJSON)},
+			}}}},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			// test data
+			eventRecorder := events.NewInMemoryRecorder("")
+			fakeKubeClient := fake.NewSimpleClientset()
+			featureGateLister := configlistersv1.NewFeatureGateLister(cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{}))
+			configSchedulerIndexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{})
+			configSchedulerIndexer.Add(&configv1.Scheduler{ObjectMeta: metav1.ObjectMeta{Name: "cluster"}, Spec: configv1.SchedulerSpec{}})
+			configSchedulerLister := configlistersv1.NewSchedulerLister(configSchedulerIndexer)
+
+			// act
+			actualConfigMap, _, err := managePod_v311_00_to_latest(
+				context.TODO(),
+				fakeKubeClient.CoreV1(),
+				fakeKubeClient.CoreV1(),
+				eventRecorder,
+				&scenario.operator.Spec.StaticPodOperatorSpec,
+				"CaptainAmerica",
+				"Piper",
+				featureGateLister,
+				configSchedulerLister)
+
+			// validate
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rawSchedulerPod, exist := actualConfigMap.Data["pod.yaml"]
+			if !exist {
+				t.Fatal("didn't find pod.yaml")
+			}
+
+			actualSchedulerPod := &corev1.Pod{}
+			if err := runtime.DecodeInto(codec, []byte(rawSchedulerPod), actualSchedulerPod); err != nil {
+				t.Fatal(err)
+			}
+
+			data := readBytesFromFile(t, scenario.goldenFile)
+			goldenSchedulerPod := &corev1.Pod{}
+			if err := runtime.DecodeInto(codec, data, goldenSchedulerPod); err != nil {
+				t.Fatal(err)
+			}
+
+			if !equality.Semantic.DeepEqual(actualSchedulerPod, goldenSchedulerPod) {
+				t.Errorf("created Scheduler Pod is different from the expected one (form a golden file) : %s", diff.ObjectDiff(actualSchedulerPod, goldenSchedulerPod))
+			}
+		})
+	}
+}
+
+func readBytesFromFile(t *testing.T, filename string) []byte {
+	file, err := os.Open(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return data
 }
