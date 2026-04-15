b3e14bd8408d65a2072f99c855900fdcf83ab6e7
Kaleemullah Siddiqui <ksiddiqu@redhat.com>
2026-04-15 18:39:25 +0530

    WRKLDS-1126:e2e network policy tests
    Signed-off-by: Kaleemullah Siddiqui <ksiddiqu@redhat.com>

---
+++ test/e2e/network_policy.go
@@ -0,0 +1,124 @@
+package e2e
+
+import (
+	"context"
+
+	g "github.com/onsi/ginkgo/v2"
+	o "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	"k8s.io/client-go/kubernetes"
+
+	test "github.com/openshift/cluster-kube-scheduler-operator/test/library"
+)
+
+const (
+	schedulerNamespace          = "openshift-kube-scheduler"
+	schedulerOperatorNamespace  = "openshift-kube-scheduler-operator"
+	defaultDenyAllPolicyName    = "default-deny-all"
+	schedulerPolicyName         = "installer-pruner"
+	schedulerOperatorPolicyName = "kube-scheduler-operator"
+)
+
+var _ = g.Describe("[sig-scheduling] kube scheduler operator", func() {
+	g.It("[NetworkPolicy][Operator][Parallel] should ensure kube scheduler NetworkPolicies are defined", func() {
+		testSchedulerNetworkPolicies()
+	})
+	g.It("[NetworkPolicy][Operator][Serial] should restore kube scheduler NetworkPolicies after delete or mutation", func() {
+		testSchedulerNetworkPolicyReconcile()
+	})
+})
+
+func testSchedulerNetworkPolicies() {
+	ctx := context.Background()
+	g.By("Creating Kubernetes clients")
+	kubeConfig, err := test.NewClientConfigForTest()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	kubeClient, err := kubernetes.NewForConfig(kubeConfig)
+	o.Expect(err).NotTo(o.HaveOccurred())
+
+	g.By("Waiting for kube scheduler operator to be ready")
+	err = test.WaitForOperator(ctx, kubeClient)
+	o.Expect(err).NotTo(o.HaveOccurred())
+
+	g.By("Validating NetworkPolicies in openshift-kube-scheduler")
+	schedulerDefaultDeny := getNetworkPolicy(ctx, kubeClient, schedulerNamespace, defaultDenyAllPolicyName)
+	logNetworkPolicySummary("scheduler/default-deny-all", schedulerDefaultDeny)
+	logNetworkPolicyDetails("scheduler/default-deny-all", schedulerDefaultDeny)
+	requireDefaultDenyAll(schedulerDefaultDeny)
+
+	schedulerPolicy := getNetworkPolicy(ctx, kubeClient, schedulerNamespace, schedulerPolicyName)
+	logNetworkPolicySummary("scheduler/installer-pruner", schedulerPolicy)
+	logNetworkPolicyDetails("scheduler/installer-pruner", schedulerPolicy)
+	requirePodSelectorMatchExpression(schedulerPolicy, "app", []string{"installer", "pruner"})
+	requireEgressPort(schedulerPolicy, corev1.ProtocolTCP, 5353)
+	requireEgressPort(schedulerPolicy, corev1.ProtocolUDP, 5353)
+	logEgressAllowAll(schedulerPolicy)
+
+	g.By("Validating NetworkPolicies in openshift-kube-scheduler-operator")
+	schedulerOperatorDefaultDeny := getNetworkPolicy(ctx, kubeClient, schedulerOperatorNamespace, defaultDenyAllPolicyName)
+	logNetworkPolicySummary("schedulerOperator/default-deny-all", schedulerOperatorDefaultDeny)
+	logNetworkPolicyDetails("schedulerOperator/default-deny-all", schedulerOperatorDefaultDeny)
+	requireDefaultDenyAll(schedulerOperatorDefaultDeny)
+
+	schedulerOperatorPolicy := getNetworkPolicy(ctx, kubeClient, schedulerOperatorNamespace, schedulerOperatorPolicyName)
+	logNetworkPolicySummary("schedulerOperator/kube-scheduler-operator", schedulerOperatorPolicy)
+	logNetworkPolicyDetails("schedulerOperator/kube-scheduler-operator", schedulerOperatorPolicy)
+	requirePodSelectorLabel(schedulerOperatorPolicy, "app", "openshift-kube-scheduler-operator")
+	requireIngressPort(schedulerOperatorPolicy, corev1.ProtocolTCP, 8443)
+	requireEgressPort(schedulerOperatorPolicy, corev1.ProtocolTCP, 5353)
+	requireEgressPort(schedulerOperatorPolicy, corev1.ProtocolUDP, 5353)
+	logEgressAllowAll(schedulerOperatorPolicy)
+
+	g.By("Verifying pods are ready in kube scheduler namespaces")
+	waitForPodsReadyByLabel(ctx, kubeClient, schedulerNamespace, "app=openshift-kube-scheduler")
+	waitForPodsReadyByLabel(ctx, kubeClient, schedulerNamespace, "app=guard")
+	waitForPodsReadyByLabel(ctx, kubeClient, schedulerOperatorNamespace, "app=openshift-kube-scheduler-operator")
+}
+
+func testSchedulerNetworkPolicyReconcile() {
+	ctx := context.Background()
+	g.By("Creating Kubernetes clients")
+	kubeConfig, err := test.NewClientConfigForTest()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	kubeClient, err := kubernetes.NewForConfig(kubeConfig)
+	o.Expect(err).NotTo(o.HaveOccurred())
+
+	g.By("Waiting for kube scheduler operator to be ready")
+	err = test.WaitForOperator(ctx, kubeClient)
+	o.Expect(err).NotTo(o.HaveOccurred())
+
+	g.By("Capturing expected NetworkPolicy specs across all namespaces")
+	expectedSchedulerPolicy := getNetworkPolicy(ctx, kubeClient, schedulerNamespace, schedulerPolicyName)
+	expectedSchedulerDefaultDeny := getNetworkPolicy(ctx, kubeClient, schedulerNamespace, defaultDenyAllPolicyName)
+	expectedSchedulerOperatorPolicy := getNetworkPolicy(ctx, kubeClient, schedulerOperatorNamespace, schedulerOperatorPolicyName)
+	expectedSchedulerOperatorDefaultDeny := getNetworkPolicy(ctx, kubeClient, schedulerOperatorNamespace, defaultDenyAllPolicyName)
+
+	g.By("Deleting all NetworkPolicies simultaneously and waiting for restoration")
+	policiesToDelete := []*networkingv1.NetworkPolicy{
+		expectedSchedulerPolicy,
+		expectedSchedulerDefaultDeny,
+		expectedSchedulerOperatorPolicy,
+		expectedSchedulerOperatorDefaultDeny,
+	}
+	deleteAndWaitForAllRestored(ctx, kubeClient, policiesToDelete)
+
+	g.By("Mutating all NetworkPolicies simultaneously and waiting for reconciliation")
+	policiesToMutate := []struct{ namespace, name string }{
+		{schedulerNamespace, schedulerPolicyName},
+		{schedulerNamespace, defaultDenyAllPolicyName},
+		{schedulerOperatorNamespace, schedulerOperatorPolicyName},
+		{schedulerOperatorNamespace, defaultDenyAllPolicyName},
+	}
+	mutateAndWaitForAllReconciled(ctx, kubeClient, policiesToMutate)
+
+	g.By("Checking NetworkPolicy-related events (best-effort)")
+	logNetworkPolicyEvents(ctx, kubeClient, []string{"openshift-kube-scheduler-operator", schedulerNamespace}, schedulerPolicyName)
+
+	g.By("Waiting for kube-scheduler ClusterOperator to stabilize after reconciliation")
+	configClient, err := test.NewConfigClient()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	err = test.WaitForClusterOperatorStable(ctx, configClient)
+	o.Expect(err).NotTo(o.HaveOccurred())
+}
